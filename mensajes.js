18f7779a1bd80d76cb7a692adf55d2f9a850e619
facundorom <facundortt@gmail.com>
2022-10-12 13:35:58 -0300

    prim-entrjs

--- mensajes.js
+++ mensajes.js
@@ -2,3 +2,15 @@ alert('Bienvenido');
 if (confirm('¿Deseas que te salude por tu nombre?')) {
     alert (`Hola ${prompt('Cual es tu nombre?')}!`);
 }
+
+let precioProd=parseFloat (prompt("Ingresa el precio del producto"));
+function calcularIva (precio){
+    return precio * 0.21
+}
+let ivaCalculado = calcularIva (precioProd);
+console.log ("EL iva =$"+ivaCalculado);
+function calcularPrecioFinal (precio, iva){
+    return precio + iva;
+}
+let precioFinal = calcularPrecioFinal (precioProd, ivaCalculado);
+console.log ("El precio final con iva incluido eds $"+precioFinal);
