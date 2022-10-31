8326ccd621869db876f481d75bfbbb893eac8de0
facundorom <facundortt@gmail.com>
2022-10-31 10:27:09 -0300

    ecomerce-1

--- arrays.js
+++ arrays.js
@@ -1,47 +1,41 @@
+const shopContent = document.getElementById("shopContent");
 
 const productos = [
     {
-        id:10,
+        id:1,
         nombre:"Crema hidratante corporal Cicatricure",
         detalle:"hidratante corporal",
-        precio:1200
+        precio:1200,
+        img:"src=./assetts/Icons/trat corp 1.png",
 
     },
     {
-        id:10,
+        id:2,
         nombre:"Hidratante corporal todo dia",
         detalle:"hidratante corporal",
-        precio:1350
+        precio:1350,
+        img:"src=./assetts/Icons/trat corp 2.jpg",
+        
 
     },
     {
-        id:10,
+        id:3,
         nombre:"Crema Cicatricure firmeza",
         detalle:"crema corporal",
-        precio:2500
-
+        precio:2500,
+        img:"src=./assetts/Icons/trat corp 3.jpg",
     },
 
 ];
-console.log(productos)
-for(const producto of productos){
-    console.log(producto.nombre+"Precio $:"+producto.precio);
-}
-class productoCream{
-    constructor(nombre, precio){
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
-        this.vendido = false;
-    }
-    sumaIva(){
-        this.precio = this.precio * 1.21;
-    }
-}
-
-const productosCream = [];
-productosCream.push(new productoCream("Crema hidratante corporal Cicatricure","1200"));
-productosCream.push(new productoCream("Hidratante corporal todo dia","1350"));
-productosCream.push(new productoCream("Crema Cicatricure firmeza","2500"));
-console.log(productosCream);
-for(const producto of productosCream)
-producto.sumaIva();
+let carrito = [] ;
+productos.forEach((product)=>{
+    let content = document.createElement("div");
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p>${product.precio} $</p>
+    `;
+    shopContent.append(content);
+    let comprar = document.createElement("button")
+    comprar.inn
+});
