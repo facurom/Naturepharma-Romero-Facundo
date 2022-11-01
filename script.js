3f473291f5b398879c1e0728186849e07a3dafac
facundorom <facundortt@gmail.com>
2022-11-01 10:02:52 -0300

    prductos.1

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ let contenedor = document.getElementById("misprods");
 function renderizarProds() {
     for (const producto of productos) {
         contenedor.innerHTML +=`
-            <div class="card" style="width: 18rem;">
+            <div class="card col-sm-3" >
              <img scr=${producto.img} class="card-img-top" alt="...">
                 <div class="card-body">
                  <h5 class="card-title">${producto.id}</h5>
                  <p class="card-text">${producto.nombre}</p>
                  <p class="card-text">${producto.precio}</p>
-                 <button class="btn btn-primary">Comprar</button>
+                 <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>
                 </div>
             </div>
         `;
     }
+    productos.forEach(producto =>{
+        document.getElementById()
+    })
 
 }
 renderizarProds();
