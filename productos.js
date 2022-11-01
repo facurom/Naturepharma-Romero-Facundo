3b468630a9a922113a327d7dca66617274135347
facundorom <facundortt@gmail.com>
2022-11-01 08:52:35 -0300

    lista-de-produc-2

--- productos.js
+++ productos.js
@@ -4,23 +4,22 @@ const productos = [{
         id: 1,
         nombre: "Crema hidratante corporal Cicatricure",
         precio: 1200,
-        img:"src=./assetts/Icons/trat corp 1.jpg",
+        img:"https://www.google.com/search?q=crema+cicatricure+hidratante+corporal&rlz=1C1KDEC_enAR923AR923&sxsrf=ALiCzsbN87oc0WnV6w9NC6Gv4TKy1R1Y5A:1667302612587&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjE8dKf8oz7AhXrrpUCHdJNBuQQ_AUoAXoECAIQAw&biw=1366&bih=617&dpr=1#imgrc=wbSDkPLwD-e-MM",
 
     },
     {
         id: 2,
         nombre: "Hidratante corporal todo dia",
         precio: 1350,
-        img: "src=./assetts/Icons/trat corp 2.jpg",
+        img: "https://www.google.com/search?q=crema+hidratante+corporal+dermaglos&tbm=isch&ved=2ahUKEwiGyriW9Yz7AhXdtJUCHdXZABAQ2-cCegQIABAA&oq=crema+hidratante+corporal+dermaglos&gs_lcp=CgNpbWcQAzIGCAAQCBAeOgQIIxAnOgUIABCABDoICAAQCBAHEB5QxSxY_DtgqD1oAHAAeACAAX-IAY4JkgEEMC4xMJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=5gdhY4a_I93p1sQP1bODgAE&bih=617&biw=1366&rlz=1C1KDEC_enAR923AR923#imgrc=9nkt5YVcYP0_PM",
 
 
     },
     {
         id: 3,
         nombre: "Crema Cicatricure firmeza",
-
         precio: 2500,
-        img: "src=./assetts/Icons/trat corp 3.jpg",
+        img: "https://www.google.com/search?q=cicatricure+crema+corporal+firmeza&tbm=isch&ved=2ahUKEwiIj7mh9Yz7AhUkr5UCHT4kAr0Q2-cCegQIABAA&oq=crema+cicatricure+firme&gs_lcp=CgNpbWcQARgBMgYIABAIEB4yBggAEAgQHjIHCAAQgAQQGDIHCAAQgAQQGDoECCMQJzoICAAQgAQQsQM6BQgAEIAEOggIABCxAxCDAVC7EljOMmDwR2gAcAB4AIABdogB6RCSAQQwLjIwmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=_QdhY8icKKTe1sQPvsiI6As&bih=617&biw=1366&rlz=1C1KDEC_enAR923AR923#imgrc=sLElFq3AZ5Be9M",
     },
 
 ];
